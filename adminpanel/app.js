00685efe762253592f64db00f7bb393abcb1204a
Fred Chien <cfsghost@gmail.com>
2016-08-27 15:07:18 +0900

    initial version

---
+++ adminpanel/app.js
@@ -0,0 +1,22 @@
+var koa = require('koa');
+var Router = require('koa-router');
+var views = require('koa-views');
+var serve = require('koa-static');
+
+var app = koa();
+var router = new Router();
+
+app.use(views(__dirname + '/views', {
+	  extension: 'pug',
+	  map: {
+		      html: 'pug'
+	  }
+}));
+
+router.get('/', function *() {
+	  yield this.render('index', { msg: 'Yeah!' });
+});
+
+app.use(router.middleware());
+app.use(serve('./public'));
+app.listen(3000);
---
+++ adminpanel/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+	entry: [
+		'./src/js/app.js'
+	],
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js',
+		publicPath: '/public/assets'
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin()
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loaders: [ 'babel' ],
+				exclude: /node_modules/,
+				query: {
+					presets: [ 'es2015', 'es2017' ]
+				}
+			},
+			{ test: /\.css$/, loader: 'style-loader!css-loader' },
+			{ test: /\.less$/, loader: 'less-loader' }
+		]
+	}
+}
